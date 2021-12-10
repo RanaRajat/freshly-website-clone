761f0080c79565c628ddc8358062a258df594717
Rajat Kumar <77433561+RanaRajat@users.noreply.github.com>
2021-12-10 09:21:00 +0530

    Update home.js

--- scripts/home.js
+++ scripts/home.js
@@ -27,7 +27,7 @@ let leftClick = document.getElementById("leftBtn");
     let menuBtn = document.getElementsByClassName("MenuBtn")[0];
 
     menuBtn.addEventListener("click",function(){
-        window.location.assign("http://127.0.0.1:5500/freshlyClone/freshly-website-clone/menuItems.html");
+        window.location.assign("freshlyClone/freshly-website-clone/menuItems.html");
     })
 
 
